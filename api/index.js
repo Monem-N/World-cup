d65220291e8c1109237cf6324ebb2e8149ab32f0
Monem-N <monem.naifer@gmail.com>
2025-05-23 12:02:33 +0100

    fix: properly configure Vercel API handler with createRequestHandler

--- api/index.js
+++ api/index.js
@@ -1,2 +1,10 @@
 // Vercel serverless function entry point for React Router v7
-export { default } from "../build/server/nodejs_eyJydW50aW1lIjoibm9kZWpzIn0/index.js";
+import { createRequestHandler } from "@react-router/node";
+import * as build from "../build/server/nodejs_eyJydW50aW1lIjoibm9kZWpzIn0/index.js";
+
+const requestHandler = createRequestHandler({
+  build,
+  mode: "production",
+});
+
+export default requestHandler;
